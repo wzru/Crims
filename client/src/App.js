df1c59f259cb46718a58d39f26ca295a12f6632a
WangZhengru <wangshaun@outlook.com>
2020-06-10 10:05:25 +0800

    feat(client): First commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Index from "./pages/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/index/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss";
+
+export default function Index() {
+  return <div className="index">
+    <div>Welcome to CRIMS</div>
+    <div className='buttons'>
+      <div>数据维护</div>
+      <div>数据查询</div>
+      <div>数据统计</div>
+    </div>
+  </div>;
+}
